ac568f0ab0315ec4c18d92982a9009fb2abb01be
ᬊAɳʝαɳ ᴅʜᴀʀ ͢ <anjandharsiliguri1@gmail.com>
2024-10-30 08:12:47 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['919883457657'] //OWNER
+global.botname = '✿᭄͜͡»͜͡𝐀𝐍𝐉𝐀𝐍»𒁍' //BOT NAME
+global.versisc = "4.4.4" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "✿᭄͜͡»͜͡𝐀𝐍𝐉𝐀𝐍»𒁍" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "✿᭄͜͡»͜͡𝐀𝐍𝐉𝐀𝐍»𒁍" //OPSIONAL
+global.simbol = "✇" // SIMBOL MENU
